50e1df7c65035d5b17d3cc84be03d5612b9857b0
Jéssica Vitoriano <103972128+imJessicaV@users.noreply.github.com>
2023-12-18 10:52:02 -0300

    Parte do curso de C# da FreeCode Camp

--- Program.cs
+++ Program.cs
@@ -2,15 +2,160 @@
 
 using CursoCSharp.Models; //Aponta onde a classe está para poder usa-la
 
-//Operadores logicos NOT
-bool Choveu = false;
-bool EstaTarde = true;
 
-if(!Choveu && !EstaTarde){
-    Console.WriteLine("Vamos pedalar");
-}else{
-    Console.WriteLine("Não vamos pedalar");
-}
+
+
+//ATIVIDADE
+// Random random = new Random();
+// int daysUntilExpiration = random.Next(12);
+// int discountPercentage = 0;
+
+// if (daysUntilExpiration == 0)
+// {
+//     Console.WriteLine("Your subscription has expired.");
+// }
+// else if (daysUntilExpiration == 1)
+// {
+//     Console.WriteLine("Your subscription expires within a day!");
+//     discountPercentage = 20;
+// }
+// else if (daysUntilExpiration <= 5)
+// {
+//     Console.WriteLine($"Your subscription expires in {daysUntilExpiration} days.");
+//     discountPercentage = 10;
+// }
+// else if (daysUntilExpiration <= 10)
+// {
+//     Console.WriteLine("Your subscription will expire soon. Renew now!");
+// }
+
+// if (discountPercentage > 0)
+// {
+//     Console.WriteLine($"Renew now and save {discountPercentage}%.");
+// }
+
+
+//IF ELSE aninhado
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     if ((roll1 == roll2) && (roll2 == roll3))
+//     {
+//         Console.WriteLine("You rolled triples!  +6 bonus to total!");
+//         total += 6;
+//     }
+//     else
+//     {
+//         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+//         total += 2;
+//     }
+// }
+
+// if (total >= 16)
+// {
+//     Console.WriteLine("You win a new car!");
+// }
+// else if (total >= 10)
+// {
+//     Console.WriteLine("You win a new laptop!");
+// }
+// else if (total == 7)
+// {
+//     Console.WriteLine("You win a trip for two!");
+// }
+// else
+// {
+//     Console.WriteLine("You win a kitten!");
+// }
+
+
+//IF ELSE
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+// {
+//     Console.WriteLine("You rolled doubles! +2 bonus to total!");
+//     total += 2;
+// }
+
+// if ((roll1 == roll2) && (roll2 == roll3)) 
+// {
+//     Console.WriteLine("You rolled triples! +6 bonus to total!");
+//     total += 6;
+// }
+
+// if (total >= 15)
+// {
+//     Console.WriteLine("You win!");
+// }
+
+// if (total < 15)
+// {
+//     Console.WriteLine("Sorry, you lose.");
+// }
+
+
+
+//IF ELSE
+// string message = "The quick brown fox jumps over the lazy dog.";
+// bool result = message.Contains("dog");
+// Console.WriteLine(result);
+
+// if (message.Contains("fox"))
+// {
+//     Console.WriteLine("What does the fox say?");
+// }
+
+//IF ELSE
+// Random dice = new Random();
+
+// int roll1 = dice.Next(1, 7);
+// int roll2 = dice.Next(1, 7);
+// int roll3 = dice.Next(1, 7);
+
+// int total = roll1 + roll2 + roll3;
+
+// Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
+// if(total > 14){
+//     Console.WriteLine("Você ganhou!!");
+// }else{
+//     Console.WriteLine("Você perdeu");
+// }
+
+//Usando Math
+// int firstValue = 500;
+// int secondValue = 600;
+// int largerValue;
+// largerValue = Math.Max(firstValue, secondValue);
+// Console.WriteLine(largerValue);
+
+//Operadores logicos NOT
+// bool Choveu = false;
+// bool EstaTarde = true;
+
+// if(!Choveu && !EstaTarde){
+//     Console.WriteLine("Vamos pedalar");
+// }else{
+//     Console.WriteLine("Não vamos pedalar");
+// }
 
 
 //Operadores logicos AND/E
